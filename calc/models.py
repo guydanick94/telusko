9aeba206b746ee6ab80bb950d9ebff25d946e757
Guydanick <guydanickmaza@gmail.com>
2020-11-05 12:56:04 +0200

    first commit

---
+++ calc/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+# class production(models.Model):
+#     title = models.CharField(max_length=255)
+#     category = models.CharField(max_length=255)
+#     farmers = models.CharField(max_length=255)
+#     amount = models.CharField(max_length=255)
+#     district = models.CharField(max_length=255)
+#     sector = models.CharField(max_length=255)
+#     cell = models.CharField(max_length=255)
+#     telephone = models.CharField(max_length=255)
+#     area = models.CharField(max_length=255)
+#     semester = models.CharField(max_length=255)
+
+#     def __str__(self):
